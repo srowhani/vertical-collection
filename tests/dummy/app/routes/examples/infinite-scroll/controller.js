a80827cc719d762d02ef082298ba15120c0a520c
Seena <seenarowhani95@gmail.com>
2017-04-14 13:42:06 -0400

    Illustration of bug

--- tests/dummy/app/routes/examples/infinite-scroll/controller.js
+++ tests/dummy/app/routes/examples/infinite-scroll/controller.js
@@ -13,26 +13,39 @@ export default Controller.extend({
 
   actions: {
 
-    loadAbove() {
-      let first = this.get('model.first');
-      let numbers = getNumbers(first - 20, 20);
-      let model = this.get('model.numbers');
-      model.unshiftObjects(numbers);
-      // this.set('model.numbers', newModel);
-      this.set('model.first', first - 20);
-    },
+    // loadAbove() {
+    //   let first = this.get('model.first');
+    //   let numbers = getNumbers(first - 20, 20);
+    //   let model = this.get('model.numbers');
+    //   model.unshiftObjects(numbers);
+    //   this.set('model.numbers', newModel);
+    //   this.set('model.first', first - 20);
+    // },
 
-    loadBelow() {
-      let last = this.get('model.last');
-      let numbers = getNumbers(last, 20);
-      let model = this.get('model.numbers');
-      model.pushObjects(numbers);
-      // this.set('model.numbers', newModel);
-      this.set('model.last', last + 20);
-    },
+    // loadBelow() {
+    //   let last = this.get('model.last');
+    //   let numbers = getNumbers(last, 20);
+    //   let model = this.get('model.numbers');
+    //   model.pushObjects(numbers);
+    //   this.set('model.numbers', newModel);
+    //   this.set('model.last', last + 20);
+    // },
 
-    setMinHeight() {
-      this.set('someProperty', 90);
+    // setMinHeight() {
+    //   this.set('someProperty', 90);
+    // },
+    removeByPop() {
+      while (!Ember.isEmpty(this.get('model.numbers'))) {
+        this.get('model.numbers').popObject();
+      }
+    },
+    removeByShift() {
+      while (!Ember.isEmpty(this.get('model.numbers'))) {
+        this.get('model.numbers').shiftObject();
+      }
+    },
+    removeByReset() {
+      this.set('model.numbers', Ember.A([]));
     }
   }
 
